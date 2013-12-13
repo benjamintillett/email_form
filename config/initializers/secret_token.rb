a7b88bed347363cf7b565e1a1530caa84bb5ad20
ben tillett <benjamin.tillett@gmail.com>
2013-12-13 08:07:08 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailForm::Application.config.secret_token = 'b6784cbf2289e441fb6ab4c96f7455e07fe47c56c690494421146c1043f7f3d00d707f6682c56e1704a190f6014059d2aa2b60506c1155fe7333337a82eae533'
